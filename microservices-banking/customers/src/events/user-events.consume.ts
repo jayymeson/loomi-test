caa4335945892a7e52afd5f3ecbe0788e684a8a1
Jaymeson Mendes <jaymesonmendes@gmail.com>
2025-02-04 08:20:54 -0300

    fix: corretion in upsert of user events

--- microservices-banking/customers/src/events/user-events.consume.ts
+++ microservices-banking/customers/src/events/user-events.consume.ts
@@ -1,21 +1,58 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
-import { UserCacheService } from '../cache/user-cache.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UserEventsConsumer {
   private readonly logger = new Logger(UserEventsConsumer.name);
 
-  constructor(private readonly userCacheService: UserCacheService) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
   @RabbitSubscribe({
     exchange: 'user-exchange',
     routingKey: 'user.created',
     queue: 'transaction-user-created',
   })
-  public async handleUserCreated(user: any) {
-    this.logger.log(`Evento recebido [user.created]: ${JSON.stringify(user)}`);
-    this.userCacheService.updateUser(user);
+  public async handleUserCreated(userPayload: any) {
+    this.logger.log(
+      `Evento recebido [user.created]: ${JSON.stringify(userPayload)}`,
+    );
+
+    const { id, name, email, bankingDetails } = userPayload;
+    await this.prismaService.user.upsert({
+      where: { id },
+      update: {
+        name,
+        email,
+        bankingDetails: bankingDetails
+          ? {
+              upsert: {
+                create: {
+                  agency: bankingDetails.agency,
+                  account: bankingDetails.account,
+                },
+                update: {
+                  agency: bankingDetails.agency,
+                  account: bankingDetails.account,
+                },
+              },
+            }
+          : undefined,
+      },
+      create: {
+        id,
+        name,
+        email,
+        bankingDetails: bankingDetails
+          ? {
+              create: {
+                agency: bankingDetails.agency,
+                account: bankingDetails.account,
+              },
+            }
+          : undefined,
+      },
+    });
   }
 
   @RabbitSubscribe({
@@ -23,8 +60,46 @@ export class UserEventsConsumer {
     routingKey: 'user.updated',
     queue: 'transaction-user-updated',
   })
-  public async handleUserUpdated(user: any) {
-    this.logger.log(`Evento recebido [user.updated]: ${JSON.stringify(user)}`);
-    this.userCacheService.updateUser(user);
+  public async handleUserUpdated(userPayload: any) {
+    this.logger.log(
+      `Evento recebido [user.updated]: ${JSON.stringify(userPayload)}`,
+    );
+
+    const { id, name, email, bankingDetails } = userPayload;
+
+    await this.prismaService.user.upsert({
+      where: { id },
+      update: {
+        name,
+        email,
+        bankingDetails: bankingDetails
+          ? {
+              upsert: {
+                create: {
+                  agency: bankingDetails.agency,
+                  account: bankingDetails.account,
+                },
+                update: {
+                  agency: bankingDetails.agency,
+                  account: bankingDetails.account,
+                },
+              },
+            }
+          : undefined,
+      },
+      create: {
+        id,
+        name,
+        email,
+        bankingDetails: bankingDetails
+          ? {
+              create: {
+                agency: bankingDetails.agency,
+                account: bankingDetails.account,
+              },
+            }
+          : undefined,
+      },
+    });
   }
 }
